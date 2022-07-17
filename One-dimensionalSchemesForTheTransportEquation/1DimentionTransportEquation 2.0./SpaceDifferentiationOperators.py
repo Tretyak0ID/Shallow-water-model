6736efa313ea56d6dad2c8d10a1608f8eef0170c
TretyakID <ilya.tretyak.02@mail.ru>
2022-07-17 20:22:24 +0300

    OOP update

--- One-dimensionalSchemesForTheTransportEquation/1DimentionTransportEquation 2.0./SpaceDifferentiationOperators.py
+++ One-dimensionalSchemesForTheTransportEquation/1DimentionTransportEquation 2.0./SpaceDifferentiationOperators.py
@@ -11,43 +11,43 @@ from pylab                import *
 class SpaceDiff:
     
     #main methods
-    def __init__(self, h, v):
-        self.h        = h
-        self.v        = v
-        self.operator = 0
+    def __init__(self, h, v, size):
+        self.h        = h                                     #шаг дискретизации по пространству
+        self.v        = v                                     #скорость
+        self.matrix   = self.MatrixGenerate(size) #матрица оператора
         
     def diff(self, f):
-        return self.operator@f
+        #диффереенцирование
+        return self.matrix@f
+
+    def MatrixGenerate(self, size):
+        #матрица опертора
+        return np.eye(size)
     
 #-CHILD-CLASES-
+#1. periodic operators
 class LeftPeriodic(SpaceDiff):
-    def __init__(self, h, v, size):
-        self.h        = h
-        self.v        = v
-
+    
+    def MatrixGenerate(self, size):
         data  = np.array([np.zeros(size)-1, np.zeros(size)+1])
         diags = np.array([-1, 0])
         D       =   sparse.spdiags(data, diags, size, size).toarray()
         D[0,-1] =  -1 #Внесение переодичности методом виртуальных точек
-        self.operator = self.v/self.h*D
+        return self.v/self.h*D
         
 class Center2Periodic(SpaceDiff):
-    def __init__(self, h, v, size):
-        self.h        = h
-        self.v        = v
-
+    
+    def MatrixGenerate(self, size):
         data  = 1/2*np.array([np.zeros(size)-1, np.zeros(size)+1])
         diags = np.array([-1, 1])
         D       =   sparse.spdiags(data, diags, size, size).toarray()
         D[0,-1] =   -1/2 #Внесение переодичности методом виртуальных точек
         D[-1,0] =    1/2 #Внесение переодичности методом виртуальных точек
-        self.operator = self.v/self.h*D
+        return self.v/self.h*D
         
 class Center4Periodic(SpaceDiff):
-    def __init__(self, h, v, size):
-        self.h        = h
-        self.v        = v
 
+    def MatrixGenerate(self, size):
         data  = 1/12*np.array([(np.zeros(size)+1), 8*(np.zeros(size)-1), 8*(np.zeros(size)+1), (np.zeros(size)-1)])
         diags = np.array([-2, -1, 1, 2])
         D       =   sparse.spdiags(data, diags, size, size).toarray()
@@ -57,4 +57,142 @@ class Center4Periodic(SpaceDiff):
         D[-2,0] =  -1/12 #Внесение переодичности методом виртуальных точек
         D[-1,0] =   8/12 #Внесение переодичности методом виртуальных точек
         D[-1,1] =  -1/12 #Внесение переодичности методом виртуальных точек
-        self.operator = self.v/self.h*D
+        return self.v/self.h*D
+
+#2. SBP
+class SBP21(SpaceDiff):
+    def H(self,size):
+        data  = np.array([np.zeros(size)+1])
+        diags = np.array([0])
+        H    = sparse.spdiags(data, diags, size, size).toarray()
+        H[ 0, 0] = 1/2
+        H[-1,-1] = 1/2
+        return H
+    
+    def Q(self,size):
+        data  = np.array([-1/2*(np.ones(size)), 1/2*(np.ones(size))])
+        diags = np.array([-1, 1])
+        Q  = sparse.spdiags(data, diags, size, size).toarray()
+        Q[ 0] = np.zeros(size)
+        Q[-1] = np.zeros(size)
+        Q[ 0, 0] = -1/2
+        Q[ 0, 1] =  1/2
+        Q[-1,-1] =  1/2
+        Q[-1,-2] = -1/2
+        return self.v/self.h*Q
+
+    def MatrixGenerate(self, size):
+        return  np.linalg.inv(self.H(size)).dot(self.Q(size))
+
+class SBP42(SpaceDiff):
+
+    def H(self,size):
+        data  = np.array([np.ones(size)])
+        diags = np.array([0])
+        H    = np.array(sparse.spdiags(data, diags, size, size).toarray(), dtype=double)
+        H[ 0,0] = 17/48
+        H[ 1,1] = 59/48
+        H[ 2,2] = 43/48
+        H[ 3,3] = 49/48
+        H[-1,-1] = H[0,0]
+        H[-2,-2] = H[1,1]
+        H[-3,-3] = H[2,2]
+        H[-4,-4] = H[3,3]
+        return H
+    
+    def Q(self,size):
+        data  = np.array([1/12*(np.zeros(size)+1), 2/3*(np.zeros(size)-1), 2/3*(np.zeros(size)+1), 1/12*(np.zeros(size)-1)])
+        diags = np.array([-2, -1, 1, 2])
+        Q  = np.array(sparse.spdiags(data, diags, size, size).toarray(), dtype=double)
+
+        Q[0,0] = -1/2
+        Q[0,1] =  59/96
+        Q[0,2] = -1/12
+        Q[0,3] = -1/32
+        Q[1,2] =  59/96
+        Q[1,3] =  0
+        Q[2,3] =  59/96
+        Q[1,0] = -Q[0,1]
+        Q[2,0] = -Q[0,2]
+        Q[3,0] = -Q[0,3]
+        Q[2,1] = -Q[1,2]
+        Q[3,1] = -Q[1,3]
+        Q[3,2] = -Q[2,3]
+        Q[-1,-1] = -Q[0,0]
+        Q[-1,-2] = -Q[0,1]
+        Q[-1,-3] = -Q[0,2]
+        Q[-1,-4] = -Q[0,3]
+        Q[-2,-3] = -Q[1,2]
+        Q[-2,-4] = -Q[1,3]
+        Q[-3,-4] = -Q[2,3]
+        Q[-2,-1] = -Q[-1,-2]
+        Q[-3,-1] = -Q[-1,-3]
+        Q[-4,-1] = -Q[-1,-4]
+        Q[-3,-2] = -Q[-2,-3]
+        Q[-4,-2] = -Q[-2,-4]
+        Q[-4,-3] = -Q[-3,-4]
+        return self.v/self.h*Q
+
+    def MatrixGenerate(self, size):
+        return np.linalg.inv(self.H(size))@self.Q(size)
+
+#3. SBP-projaction methods
+class SBP21PROJ(SBP21):
+
+    def P(self, size):
+        A = np.zeros(size)+1
+        data  = np.array([A])
+        diags = np.array([0])
+        P     = sparse.spdiags(data, diags, size, size).toarray()
+        P[ 0, 0] = 1/2
+        P[ 0,-1] = 1/2
+        P[-1,-1] = 1/2
+        P[-1, 0] = 1/2
+        return P
+
+    def MatrixGenerate(self, size):
+        return self.P(size)@np.linalg.inv(self.H(size))@self.Q(size)
+
+class SBP42PROJ(SBP42):
+
+    def P(self, size):
+        A = np.zeros(size)+1
+        data  = np.array([A])
+        diags = np.array([0])
+        P     = sparse.spdiags(data, diags, size, size).toarray()
+        P[ 0, 0] = 1/2
+        P[ 0,-1] = 1/2
+        P[-1,-1] = 1/2
+        P[-1, 0] = 1/2
+        return P
+
+    def MatrixGenerate(self, size):
+        return self.P(size)@np.linalg.inv(self.H(size))@self.Q(size)
+
+#4. SBP-SAT methods
+class SBP21SAT(SBP21):
+
+    def SAT(self, f):
+        #SAT-добавки в оператор
+        e0 = np.zeros(f.size)
+        eN = np.zeros(f.size)
+        e0[ 0] = 1
+        eN[-1] = 1
+        return 1/2*(f[0]-f[-1])*(e0+eN)
+
+    def diff(self, f):
+        return self.matrix@f + self.v*np.linalg.solve(self.H(f.size), self.SAT(f))
+
+class SBP42SAT(SBP42):
+
+    def SAT(self, f):
+        #SAT-добавки в оператор
+        e0 = np.zeros(f.size)
+        eN = np.zeros(f.size)
+        e0[ 0] = 1
+        eN[-1] = 1
+        return 1/2*(f[0]-f[-1])*(e0+eN)
+
+    def diff(self, f):
+        return self.matrix@f + self.v*np.linalg.solve(self.H(f.size),self.SAT(f))
+
--- One-dimensionalSchemesForTheTransportEquation/1DimentionTransportEquation 2.0./TimeDifferentiationOperators.py
+++ One-dimensionalSchemesForTheTransportEquation/1DimentionTransportEquation 2.0./TimeDifferentiationOperators.py
@@ -4,38 +4,87 @@ import scipy
 
 from scipy                import sparse
 from scipy.sparse         import linalg
-from numpy                import pi, sin, cos, ma
+from numpy                import pi, sin, cos, ma, sqrt
 from pylab                import *
 
 #-MAIN-CLASS-
 class TimeDiff:
     
-    def __init__(self, tau, v, time_operator): 
+    #main methods
+    def __init__(self, tau, space_operator): 
         self.tau            = tau
-        self.v              = v
-        self.time_operator  = time_operator
+        self.v              = space_operator.v
+        self.space_operator = space_operator
         
     def make_step(self, f):
-        return self.time_operator@f[j,:]
+        #один шаг по времени
+        return f
         
     def diff(self, f):
+        #полное дифференцирование по времени
         for j in range(f[:,0].size-1):
-            f[j+1,:] = make_step(f[j,:])
-    
-    
+            f[j+1,:] = self.make_step(f[j,:])
+
+    #Методы-анализа-ошибок
+    def CourantNumber(self):
+        print("CourantNumber: " + str(self.v*self.tau/self.space_operator.h))
+
+    def l2Norm(self, f):
+        #отслеживание l2-нормы (закон сохранения энергии)
+        l2 = np.zeros(f[:,0].size)
+        for j in range(f[:,0].size):
+            l2[j] = abs(f[j,:]@self.space_operator.diff(f[j,:]))*self.space_operator.h
+        return l2
+
+    def l2HNorm(self, f):
+        #отслеживание l2-нормы (закон сохранения энергии)
+        l2 = np.zeros(f[:,0].size)
+        M = self.space_operator.H(f[0,:].size)
+        for j in range(f[:,0].size):
+            l2[j] = abs(f[j,:]@M@self.space_operator.diff(f[j,:]))*self.space_operator.h
+        return l2
+
+    def l1Norm(self, f):
+        #отслеживание сохранения интеграла (закон сохранения массы)
+        l1 = np.zeros(f[:,0].size)
+        for j in range(f[:,0].size):
+            l1[j] = abs(np.ones(f[j,:].size)@self.space_operator.diff(f[j,:]))
+        return l1
+
+    def l1HNorm(self, f):
+        #отслеживание сохранения интеграла (закон сохранения массы)
+        l1 = np.zeros(f[:,0].size)
+        M = self.space_operator.H(f[0,:].size)
+        for j in range(f[:,0].size):
+            l1[j] = abs(np.ones(f[j,:].size)@M@self.space_operator.diff(f[j,:]))
+        return l1
+
+    def l2SBP(self,f):
+        l2 = np.zeros(f[:,0].size)
+        M = self.space_operator.H(f[0,:].size)
+        for j in range(f[:,0].size):
+            l2[j] = abs(f[j,:]@M@self.space_operator.matrix@f[j,:])
+        return l2
+
+    def l2SAT(self,f):
+        l2 = np.zeros(f[:,0].size)
+        M = self.space_operator.H(f[0,:].size)
+        for j in range(f[:,0].size):
+            l2[j] = abs(f[j,:]@self.space_operator.SAT(f[j,:]))
+        return l2
+
+#-CHILD-CLASES-
+#   
 class Euler(TimeDiff):
-    def __init__(self, tau, v, space_operarot): 
-        self.tau            = tau
-        self.v              = v
-        self.time_operator = np.eye(space_operator[0,:].size) - tau*space_operator
+
+    def make_step(self, f):
+        return f + self.tau*self.space_operator.diff(f)
         
 class RK4(TimeDiff):
-    def __init__(self, tau, v, space_operator): 
-        self.tau            = tau
-        self.v              = v
-        
-        D1 = space_operator
-        D2 = D1@D1
-        D3 = D1@D1@D1
-        D4 = D1@D1@D1@D1
-        self.time_operator = np.eye(space_operator[0,:].size) - tau*D1 - tau**2/2*D2 - tau**3/6*D3 - tau**4/24*D4
+
+    def make_step(self, f):
+        f1 = -self.space_operator.diff(f)
+        f2 = -self.space_operator.diff(f1)
+        f3 = -self.space_operator.diff(f2)
+        f4 = -self.space_operator.diff(f3)
+        return f + + self.tau*f1 + self.tau**2/2*f2 + self.tau**3/6*f3 + self.tau**4/24*f4
