8cf1ccfe8229717f8955795f5998bd943317744c
Tretyak0ID <Tretyak0ID@gmail.com>
2022-09-16 13:04:29 +0300

    Revert "update geostrophic balance"
    This reverts commit 7ed510f8c7ebfc77acf9077e2a5cfaec7935c0b5.

--- 2DSWM/initial_conditions.py
+++ 2DSWM/initial_conditions.py
@@ -41,10 +41,8 @@ def eddy_geostrophic_balance(domain, pcori, g, h_mean, scale_h, scale_sigma):
     state.u = - g / pcori * h_mean * scale_h * np.exp( - dx ** 2 - dy ** 2) * 2 * dy / scale_sigma
     return state
 
-
 def eddy_full_geostrophic_balance(domain, pcori, g, h_mean, scale_h, scale_sigma):
     state = State.zeros(domain.nx, domain.ny)
-    
     r = np.sqrt((domain.xx - np.mean(domain.x)) ** 2 + (domain.yy - np.mean(domain.y)) ** 2)
     phi = np.zeros_like(r)
     for i in range(domain.nx):
@@ -56,26 +54,6 @@ def eddy_full_geostrophic_balance(domain, pcori, g, h_mean, scale_h, scale_sigma
     state.h = h_mean - (h_mean * np.exp( - (r / scale_sigma) ** 2)) * scale_h
     state.u = - vtan * np.cos(np.pi / 2.0 - phi)
     state.v =   vtan * np.sin(np.pi / 2.0 - phi) 
-    return state
-
-
-def eddy_and_velocity_geostrophic_balance(domain, pcori, g, h_mean, scale_h, scale_sigma):
-    state = State.zeros(domain.nx, domain.ny)
-    r = np.sqrt((domain.xx - np.mean(domain.x)) ** 2 + (domain.yy - np.mean(domain.y)) ** 2)
-    phi = np.zeros_like(r)
-    for i in range(domain.nx):
-        for j in range(domain.ny):
-            phi[j,i] = math.atan2((domain.yy[j,i] - np.mean(domain.y)), (domain.xx[j,i] - np.mean(domain.x)))
-    dhdr = h_mean * np.exp( - (r / scale_sigma) ** 2) * scale_h * 2.0 * r / scale_sigma ** 2
-    vtan = (- r * pcori + np.sqrt((r * pcori) ** 2 + 4.0 * g * r * dhdr)) / 2.0
-
-    h0 = np.cos((domain.yy + 10**7) / (6371.22 * 1000.0)) * (6371.22 * 1000.0) * pcori / g * 10
-    u0 = np.sin((domain.yy + 10**7) / (6371.22 * 1000.0)) * 10
-    v0 = 0
-
-    state.h = h_mean - (h_mean * np.exp( - (r / scale_sigma) ** 2)) * scale_h + h0
-    state.u = - vtan * np.cos(np.pi / 2.0 - phi) + u0
-    state.v =   vtan * np.sin(np.pi / 2.0 - phi) + v0
     return state
 
 
